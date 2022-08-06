c116681cf6e4ad438744e8085de148a7b995743f
Atanasoaie Dan <danatanasoaie17@gmail.com>
2022-08-06 16:45:37 +0300

    get HTMLfunction

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
+function getTeamHTML(team) {
+  return;
+  ` <tr>
+  <td>${team.promotion}</td>
+  <td>${team.members}</td>
+  <td>${team.name}</td>
+  <td>
+    <a href="${team.url}"
+      >github</a
+    >
+  </td>
+  <td>x e</td>
+</tr>`;
+}
+
 function displayTeams(teams) {
+  // transforma in html
   var teamsHTML = "";
   teams.forEach(function (team) {
-    console.info(team);
-    teamsHTML += ` 
-    <tr>
-    <td>${team.promotion}</td>
-    <td>${team.members}</td>
-    <td>${team.name}</td>
-    <td>
-      <a href="${team.url}"
-        >github</a
-      >
-    </td>
-    <td>x e</td>
-  </tr>`;
+    teamsHTML += getTeamHTML(team);
   });
   document.querySelector("table tbody").innerHTML = teamsHTML;
 }
