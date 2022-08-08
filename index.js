fba2109f42525388d10baefecf930cf09e2dc808
Atanasoaie Dan <danatanasoaie17@gmail.com>
2022-08-08 20:41:15 +0300

    prepare submit team

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ function getTeamHTML(team) {
     <td>${team.name}</td>
     <td>
       <a href="${team.url}"
-        >github</a
-      >
+        >open</a>
     </td>
     <td>x e</td>
   </tr>`;
@@ -34,4 +33,27 @@ function loadTeams() {
       displayTeams(teams);
     });
 }
+
+function submitForm(e) {
+  e.preventDefault();
+  var promotion = document.querySelector("input[name=promotion]").value;
+  var members = document.querySelector("input[name=members]").value;
+  var name = document.querySelector("input[name=name]").value;
+  var url = document.querySelector("input[name=url]").value;
+
+  var team = {
+    promotion: promotion,
+    members: members,
+    name: name,
+    url: url,
+  };
+
+  console.warn("submit", JSON.stringify(team));
+}
+
+function initEvents() {
+  var form = document.getElementById("editForm");
+  form.addEventListener("submit", submitForm);
+}
 loadTeams();
+initEvents();
